557c2c5dd9edfbd852770fc3b2f7ef1ef2b28ade
LornaDeng <w2deng@ucsd.edu>
2026-08-26 21:16:25 -0700

    Add Android 8 favorite tap and Room database

--- app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/laioffer/spotify/ui/playlist/PlaylistScreen.kt
@@ -1,6 +1,8 @@
 package com.laioffer.spotify.ui.playlist
 
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -38,15 +40,24 @@ import com.laioffer.spotify.datamodel.Song
 @Composable
 fun PlaylistScreen(playlistViewModel: PlaylistViewModel) {
     val playlistUiState by playlistViewModel.uiState.collectAsState()
-    PlaylistScreenContent(playlistUiState = playlistUiState)
+    PlaylistScreenContent(
+        playlistUiState = playlistUiState,
+        onTapFavorite = { isFavorite ->
+            Log.d("PlaylistScreen", "Tap favorite $isFavorite")
+        }
+    )
 }
 
 @Composable
-private fun PlaylistScreenContent(playlistUiState: PlaylistUiState) {
+private fun PlaylistScreenContent(
+    playlistUiState: PlaylistUiState,
+    onTapFavorite: (Boolean) -> Unit
+) {
     Column(modifier = Modifier.padding(16.dp)) {
         Cover(
             album = playlistUiState.album,
-            isFavorite = playlistUiState.isFavorite
+            isFavorite = playlistUiState.isFavorite,
+            onTapFavorite = onTapFavorite
         )
         PlaylistHeader(album = playlistUiState.album)
         PlaylistContent(playlist = playlistUiState.playlist)
@@ -111,13 +122,18 @@ private fun PlaylistHeader(album: Album) {
 }
 
 @Composable
-private fun Cover(album: Album, isFavorite: Boolean) {
+private fun Cover(
+    album: Album,
+    isFavorite: Boolean,
+    onTapFavorite: (Boolean) -> Unit
+) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Box(modifier = Modifier.fillMaxWidth()) {
             Icon(
                 modifier = Modifier
                     .size(28.dp)
-                    .align(Alignment.TopEnd),
+                    .align(Alignment.TopEnd)
+                    .clickable { onTapFavorite(!isFavorite) },
                 painter = painterResource(
                     if (isFavorite) R.drawable.ic_favorite_24
                     else R.drawable.ic_unfavorite_24
